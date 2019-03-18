eb0a47e9a8dd4ff74dc2becb31b649b09a6cec93
QUICKADMINPANEL <info@laraveldaily.com>
2019-03-18 10:58:40 +0000

    QuickAdminPanel Deployment ID:2518 on 2019-03-18 12:58:40

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,54 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-10">
+         <div class="col-md-6">
             <div class="panel panel-default">
-                <div class="panel-heading">@lang('global.app_dashboard')</div>
+                <div class="panel-heading">Recently added posts</div>
 
-                <div class="panel-body">
-                    @lang('global.app_dashboard_text')
+                <div class="panel-body table-responsive">
+                    <table class="table table-bordered table-striped ajaxTable">
+                        <thead>
+                        <tr>
+                            
+                            <th> @lang('global.posts.fields.created')</th> 
+                            <th> @lang('global.posts.fields.description')</th> 
+                            <th>&nbsp;</th>
+                        </tr>
+                        </thead>
+                        @foreach($posts as $post)
+                            <tr>
+                               
+                                <td>{{ $post->created }} </td> 
+                                <td>{{ $post->description }} </td> 
+                                <td>
+
+                                    @can('post_view')
+                                    <a href="{{ route('admin.posts.show',[$post->id]) }}" class="btn btn-xs btn-primary">@lang('global.app_view')</a>
+                                    @endcan
+
+                                    @can('post_edit')
+                                    <a href="{{ route('admin.posts.edit',[$post->id]) }}" class="btn btn-xs btn-info">@lang('global.app_edit')</a>
+                                    @endcan
+
+                                    @can('post_delete')
+{!! Form::open(array(
+                                        'style' => 'display: inline-block;',
+                                        'method' => 'DELETE',
+                                        'onsubmit' => "return confirm('".trans("global.app_are_you_sure")."');",
+                                        'route' => ['admin.posts.destroy', $post->id])) !!}
+                                    {!! Form::submit(trans('global.app_delete'), array('class' => 'btn btn-xs btn-danger')) !!}
+                                    {!! Form::close() !!}
+                                    @endcan
+                                
+</td>
+                            </tr>
+                        @endforeach
+                    </table>
                 </div>
             </div>
-        </div>
+ </div>
+
+
     </div>
 @endsection
+
